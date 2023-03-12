05f79bef52a1e3c62cc5ae28d8ffba4a09e5567a
Georgina Edwards <118028272+Notunique1@users.noreply.github.com>
2023-03-12 15:18:00 -0400

    first commit

--- assessment-backend/server/controller.js
+++ assessment-backend/server/controller.js
@@ -1,16 +1,43 @@
-module.exports = {
-
-    getCompliment: (req, res) => {
+module.exports = { b
+                                         
+    getCompliment: (req, res) =>  res.status(200).send(randomCompliment),
+     {
         const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
       
         
         // choose random compliment
-        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomIndex = Math.floor(Math.random () * compliments.length);
         let randomCompliment = compliments[randomIndex];
       
-        res.status(200).send(randomCompliment);
+       
+    };
     },
+
+    module.exports = {
     getFortune: (req, res) => {
         const getFortune = ["You love chinese food.", "You look pretty.", "You love challenge.", "You never know who you touch.","You have yearning for perfection."];
 }
+let randomIndex = Math.floor(Math.random() * fortunes.length);
+let randomFortune = fortune[randomIndex]
+
+res.status(200).send(this.randomFortune)
+
 }
+module.exports = {
+    getFortune: (req, res) => {
+        const getFortune = ["You love chinese food.", "You look pretty.", "You love challenge.", "You never know who you touch.","You have yearning for perfection."];
+}
+let randomIndex = Math.floor(Math.random() * fortunes.length);
+let randomFortune = fortune[randomIndex]
+
+res.status(200).send(this.randomFortune)
+
+}
+module.exports = {
+    getFortune: (req, res) => {
+        const getFortune = ["You love chinese food.", "You look pretty.", "You love challenge.", "You never know who you touch.","You have yearning for perfection."];
+}
+let randomIndex = Math.floor(Math.random() * fortunes.length);
+let randomFortune = fortune[randomIndex]
+
+res.status(200).send(this.randomFortune)
--- assessment-backend/server/index.js
+++ assessment-backend/server/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+
 const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors());                 
 
 app.use(express.json());
 
@@ -17,3 +18,84 @@ const port = 4000;
 app.listen(4000, () => console.log("Server running on 4000"));
 
 
+const express = require("express");
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());                 
+
+app.use(express.json());
+
+const { getCompliment } = require('./controller')
+let getCompliment = [
+
+    "You love chinese food.", "You look pretty.", "You love challenge.", "You never know who you touch.","You have yearning for perfection."
+];
+app.get("/api/compliment", getCompliment);
+const port = 4000;
+app.listen(4000, () => console.log("Server running on 4000"));
+
+
+
+const express = require("express");
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());                 
+
+app.use(express.json());
+
+const { getCompliment } = require('./controller')
+let getCompliment = [
+
+    "You love chinese food.", "You look pretty.", "You love challenge.", "You never know who you touch.","You have yearning for perfection."
+];
+app.get("/api/compliment", getCompliment);
+const port = 4000;
+app.listen(4000, () => console.log("Server running on 4000"));
+
+
+
+const express = require("express");
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());                 
+
+app.use(express.json());
+
+const { getCompliment } = require('./controller')
+let getCompliment = [
+
+    "You love chinese food.", "You look pretty.", "You love challenge.", "You never know who you touch.","You have yearning for perfection."
+];
+app.get("/api/compliment", getCompliment);
+const port = 4000;
+app.listen(4000, () => console.log("Server running on 4000"));
+
+
+
+const express = require("express");
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());                 
+
+app.use(express.json());
+
+const { getCompliment } = require('./controller')
+let getCompliment = [
+
+    "You love chinese food.", "You look pretty.", "You love challenge.", "You never know who you touch.","You have yearning for perfection."
+];
+app.get("/api/compliment", getCompliment);
+const port = 4000;
+app.listen(4000, () => console.log("Server running on 4000"));
